3c35b10196dbfa96c96c76974e96437c371a3601
sasa <saskelecevic@gmail.com>
2016-06-11 20:43:52 +0200

    Initial commit

--- run_all_tests.py
+++ run_all_tests.py
@@ -2,6 +2,7 @@ import unittest
 import test.test_testdialog as test_testdialog
 import test.test_extractor as test_extractor
 
+# all test suites
 suite1 = test_testdialog.suite()
 suite2 = test_extractor.suite()
 all_tests = unittest.TestSuite((suite1, suite2))
